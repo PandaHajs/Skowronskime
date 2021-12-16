57479812eec43cc7a9d9edc668d533056e190fe7
unknown <radyoczlonek@gmail.com>
2021-12-16 09:28:26 +0100

    Added BUTTONS

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
 
       <p>I don't really know how you got here... but here goes</p>
       <div class="box">
-        <img src="../koto.png">
+        <img src="../Koto.png" class="img">
         <p>My first name's Casper but with a K and a C, 
           middle name's Maria, yes it can be a man's 
           name too, my surname's Skowroński
@@ -13,13 +13,22 @@
           “If I don't have to do it, I won't. If I have to do it, I'll make it quick.”, 
           who for some reason made this site without having to</p>
       </div>
+      <div id="box3">
+        <button class="btn"><a href=""><img src="../discord.png"></a></button>
+        <button class="btn"><a href="https://github.com/PandaHajs"><img src="../GitHub-Emblem.png"></a></button>
+        <button class="btn"><a href=""><img src=""></a></button>
+        <button class="btn"><a href=""><img src=""></a></button>
+        <button class="btn"><a href=""><img src=""></a></button>
+        <button class="btn"><a href=""><img src=""></a></button>
+      </div>
+
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Title'
+    title: 'Main'
   }
 }
 </script>
@@ -35,9 +44,31 @@ export default {
   grid-gap: 20px 20px;
 }
 
-img{
+.img{
   border-radius: 100%;
   height: 11rem;
   width: 11rem;
+}
+
+#box3{
+  display: grid;
+  justify-content: right;
+  grid-template-columns: auto auto auto;
+  grid-gap: 20px 20px;
+}
+
+.btn{
+  width: 80px;
+  height: 30px;
+  border-radius: 15px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+img{
+  width: 60px;
+  height: 30px;
+  display: flex;
 }
 </style>
