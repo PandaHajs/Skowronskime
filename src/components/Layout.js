97345e84fa0c62fb1fb37e1b1b57f66e2f95d513
PandaHajs <kacpermariaskowronski@protonmail.com>
2022-12-07 10:26:31 +0100

    did nothing cause vercel doesn't want to deploy for some reason

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-
 export default function Layout({ children }) {
   const [isDrop, setIsDrop] = useState(false);
   function drop() {
