cb1e83bfcd291d0c22f91e920dc0766756d32b77
Kacper Skowroński <kacpermariaskowronski@protonmail.com>
2023-02-20 14:39:23 +0100

    Source code link fixed color and some text fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,10 +14,10 @@ const About: AboutComponent = () => {
           I was born on [REDACTED] in [REDACTED], I&apos;m currently {age} years
           old and living in Poznan, Poland. At this point in time I have only
           primary education and finished more than half of my High School. I
-          have the CISCO ITE 5.0 certificate and I&apos;m undergoing the CISCO
-          CCNA1 course. I&apos;m not yet sure what I want to do in life, but
-          I&apos;m certain it will either be something in networking or web
-          development.
+          have the CISCO ITE 5.0 certificate and succesfully underwent the CISCO
+          CCNA1 course (I&apos;m not certified tho). I&apos;m not yet sure what
+          I want to do in life, but I&apos;m certain it will either be something
+          in networking or web development.
         </p>
       </div>
       <div className={styles.projects}>
